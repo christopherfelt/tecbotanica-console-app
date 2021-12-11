52446ae55fe756f7abbcefc21a949801374312cb
christopherfelt <chris.h.felt@gmail.com>
2021-12-11 12:33:04 -0700

    basic logic established

--- pumps.js
+++ pumps.js
@@ -2,50 +2,54 @@ const inquirer = require("inquirer");
 
 let runProgram = true;
 
-// async function question1() {
-//   let answer = await inquirer
-    // .prompt([
-    //   {
-    //     name: "pump",
-    //     message: "Which is pump is it?",
-    //   },
-    // ])
-//     .then((answers) => {
-//       console.log("Answer: ", answers.pump);
-//       runProgram = false;
-//     });
-// }
+let pinMap = {
+  Potato_1: 1,
+  Potato_2: 2, 
+  Onion_1: 3, 
+  Onion_2: 4,
+  Carrot: 5,
+}
 
-// function main() {
-//   while (runProgram) {
-//     question1();
-//   }
-// }
-
-// main();
+async function main(){
+  let result = "";
+  while(runProgram){
+      await getAnswer()
+      .then((answer) =>{
+        // NOTE: Cannot put activatePump function in here.  It does not wait. Await cannot also be used.  Is 'then' not an async function?
+          if(answer.pump != "Exit"){
+            result = answer.pump;
+          } else {
+              runProgram = false;
+          }
+      });
+      if(!runProgram) break;
+      await activatePump(result);
+  }
+}
 
 function getAnswer() {
   return inquirer.prompt([
     {
+      type: 'list',
       name: "pump",
       message: "Which is pump is it?",
+      choices: [...Object.keys(pinMap), 'Exit' ],
     },
-  ]);
+  ])
 
-//   return answer.pumps;
 }
 
 
-async function main(){
-    while(runProgram){
-        await getAnswer().then((answer) =>{
-            if(answer.pump < 5){
-                console.log(answer.pump);
-            } else {
-                runProgram = false;
-            }
-        });
-    }
+function activatePump(pump){
+  // console.log(`Pump ${pump} on`);
+  // console.log(`Activating Pin ${pinMap[pump]}`);
+  // setTimeout(()=>{
+  //   console.log(`Pump ${pump} off`);
+  // }, 1000);
+  console.log(`Pump ${pump} on`);
+  console.log(`Activating Pin ${pinMap[pump]}`);
+  return new Promise(resolve => setTimeout(() => resolve(console.log(`Pump ${pump} off`)), 1000))
+
 }
 
 
